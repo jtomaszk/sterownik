2abb7b9df0531a47e4e520c8edca82897e4f0fdf
warthel <warthel@users.noreply.github.com>
2014-09-25 00:10:56 +0200

    Update arduino_sterownik.ino

--- arduino_sterownik/arduino_sterownik.ino
+++ arduino_sterownik/arduino_sterownik.ino
@@ -135,3 +135,28 @@ void loop(void){
     }
   }
 }
+
+
+/*
+
+#include "DHT.h"
+
+DHT dht;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  dht.setup(2); // data pin 2
+}
+
+void loop()
+{
+  delay(dht.getMinimumSamplingPeriod());
+
+  Serial.print(dht.getHumidity());
+  Serial.print("\t");
+  Serial.print(dht.getTemperature());
+  Serial.print("\n");
+}
+*/
