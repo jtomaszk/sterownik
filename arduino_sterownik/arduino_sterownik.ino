b07adf1fd55343cbf0f3920fbb112af90f3b0f1c
warthel <warthel@users.noreply.github.com>
2014-09-25 21:02:26 +0200

    Update arduino_sterownik.ino

--- arduino_sterownik/arduino_sterownik.ino
+++ arduino_sterownik/arduino_sterownik.ino
@@ -35,16 +35,21 @@ void setup() {
   
   radio.startListening();                 // Start listening
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
+
+  Serial.println(dht.getStatusString());
+  
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
 }
 
-void write(char[] msg) {
+void write(char msg[]) {
   radio.stopListening();
   
   printf("Now sending \n\r");
 
   unsigned long time = micros();                             // Take the time, and send it.  This will block until complete
-  if (!radio.write( &time, sizeof(unsigned long) )){  printf("failed.\n\r");  }
-      
+  if (!radio.write( msg, 255 )){  printf("failed.\n\r");  }
+//  if (!radio.write( &time, sizeof(unsigned long) )){  printf("failed.\n\r");  }    
   radio.startListening();                                    // Now, continue listening
   
   unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
@@ -68,22 +73,129 @@ void write(char[] msg) {
   }
 }
 
-void readTemp(char[] & out) {
-  Serial.print(dht.getHumidity());
-  Serial.print("\t");
-  Serial.print(dht.getTemperature());
-  Serial.print("\n");
+void fmtDouble(double val, byte precision, char *buf, unsigned bufLen = 0xffff);
+unsigned fmtUnsigned(unsigned long val, char *buf, unsigned bufLen = 0xffff, byte width = 0);
+
+//
+// Produce a formatted string in a buffer corresponding to the value provided.
+// If the 'width' parameter is non-zero, the value will be padded with leading
+// zeroes to achieve the specified width.  The number of characters added to
+// the buffer (not including the null termination) is returned.
+//
+unsigned fmtUnsigned(unsigned long val, char *buf, unsigned bufLen, byte width)
+{
+  if (!buf || !bufLen)
+    return(0);
+
+  // produce the digit string (backwards in the digit buffer)
+  char dbuf[10];
+  unsigned idx = 0;
+  while (idx < sizeof(dbuf))
+  {
+    dbuf[idx++] = (val % 10) + '0';
+    if ((val /= 10) == 0)
+      break;
+  }
+
+  // copy the optional leading zeroes and digits to the target buffer
+  unsigned len = 0;
+  byte padding = (width > idx) ? width - idx : 0;
+  char c = '0';
+  while ((--bufLen > 0) && (idx || padding))
+  {
+    if (padding)
+      padding--;
+    else
+      c = dbuf[--idx];
+    *buf++ = c;
+    len++;
+  }
+
+  // add the null termination
+  *buf = '\0';
+  return(len);
+}
+
+// Format a floating point value with number of decimal places.
+// The 'precision' parameter is a number from 0 to 6 indicating the desired decimal places.
+// The 'buf' parameter points to a buffer to receive the formatted string.  This must be
+// sufficiently large to contain the resulting string.  The buffer's length may be
+// optionally specified.  If it is given, the maximum length of the generated string
+// will be one less than the specified value.
+//
+// example: fmtDouble(3.1415, 2, buf); // produces 3.14 (two decimal places)
+//
+void fmtDouble(double val, byte precision, char *buf, unsigned bufLen)
+{
+  if (!buf || !bufLen)
+    return;
+
+  // limit the precision to the maximum allowed value
+  const byte maxPrecision = 6;
+  if (precision > maxPrecision)
+    precision = maxPrecision;
+
+  if (--bufLen > 0)
+  {
+    // check for a negative value
+    if (val < 0.0)
+    {
+      val = -val;
+      *buf = '-';
+      bufLen--;
+    }
+
+    // compute the rounding factor and fractional multiplier
+    double roundingFactor = 0.5;
+    unsigned long mult = 1;
+    for (byte i = 0; i < precision; i++)
+    {
+      roundingFactor /= 10.0;
+      mult *= 10;
+    }
+
+    if (bufLen > 0)
+    {
+      // apply the rounding factor
+      val += roundingFactor;
+
+      // add the integral portion to the buffer
+      unsigned len = fmtUnsigned((unsigned long)val, buf, bufLen);
+      buf += len;
+      bufLen -= len;
+    }
+
+    // handle the fractional portion
+    if ((precision > 0) && (bufLen > 0))
+    {
+      *buf++ = '.';
+      if (--bufLen > 0)
+        buf += fmtUnsigned((unsigned long)((val - (unsigned long)val) * mult), buf, bufLen, precision);
+    }
+  }
+
+  // null-terminate the string
+  *buf = '\0';
+}
+
+void readTemp(char out[]) {
+  sprintf(out, "H=");
+  fmtDouble(dht.getHumidity(), 4, out+2, 9);
+  sprintf(out+9, ";T=");  
+  fmtDouble(dht.getTemperature(), 4, out+12, 10);
+  sprintf(out+19, ";\n");  
 }
 
 void loop(void){
-    char[255] msg;
+    char msg[255] = "";
     
+    delay(dht.getMinimumSamplingPeriod());
     readTemp(msg);
+    Serial.print(msg);
     write(msg);
     
-    delay(1000);
+    delay(2000);
 }
 
 //  delay(dht.getMinimumSamplingPeriod());
-
 
